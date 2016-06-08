374c090166f7dc728048d6f55d972af43737d75c
b7song <b7song@student.cs.uwaterloo.ca>
2016-06-07 20:10:24 -0400

    End of A1, moving onto A2

--- os161-1.99/kern/synchprobs/traffic_synch.c
+++ os161-1.99/kern/synchprobs/traffic_synch.c
@@ -18,14 +18,21 @@
  * declare other global variables if your solution requires them.
  */
 
-/*
- * replace this with declarations of any synchronization and other variables you need here
- */
-static struct semaphore *intersectionSem;
+static struct lock *intersection;
+static struct lock *action;
+static struct cv *paths[33];
+int volatile count = 0;
+struct linked_list {
+  int path;
+  struct linked_list *next;
+};
+typedef struct linked_list node;
+static volatile node *head;
+static volatile node *tail;
+bool volatile list_created = false;
+int volatile numCars = 0;
 
-
-/* 
- * The simulation driver will call this function once before starting
+/* The simulation driver will call this function once before starting
  * the simulation
  *
  * You can use it to initialize synchronization and other variables.
@@ -34,12 +41,20 @@ static struct semaphore *intersectionSem;
 void
 intersection_sync_init(void)
 {
-  /* replace this default implementation with your own implementation */
-
-  intersectionSem = sem_create("intersectionSem",1);
-  if (intersectionSem == NULL) {
-    panic("could not create intersection semaphore");
-  }
+  intersection = lock_create("intersection");
+  action = lock_create("action");
+  paths[1] = cv_create("ne");
+  paths[2] = cv_create("ns");
+  paths[3] = cv_create("nw");
+  paths[10] = cv_create("en");
+  paths[12] = cv_create("es");
+  paths[13] = cv_create("ew");
+  paths[20] = cv_create("sn");
+  paths[21] = cv_create("se");
+  paths[23] = cv_create("sw");
+  paths[30] = cv_create("wn");
+  paths[31] = cv_create("we");
+  paths[32] = cv_create("ws");
   return;
 }
 
@@ -53,9 +68,20 @@ intersection_sync_init(void)
 void
 intersection_sync_cleanup(void)
 {
-  /* replace this default implementation with your own implementation */
-  KASSERT(intersectionSem != NULL);
-  sem_destroy(intersectionSem);
+  lock_destroy(action);
+  cv_destroy(paths[1]);
+  cv_destroy(paths[2]);
+  cv_destroy(paths[3]);
+  cv_destroy(paths[10]);
+  cv_destroy(paths[12]);
+  cv_destroy(paths[13]);
+  cv_destroy(paths[20]);
+  cv_destroy(paths[21]);
+  cv_destroy(paths[23]);
+  cv_destroy(paths[30]);
+  cv_destroy(paths[31]);
+  cv_destroy(paths[32]);
+  lock_destroy(intersection);
 }
 
 
@@ -75,11 +101,50 @@ intersection_sync_cleanup(void)
 void
 intersection_before_entry(Direction origin, Direction destination) 
 {
-  /* replace this default implementation with your own implementation */
-  (void)origin;  /* avoid compiler complaint about unused parameter */
-  (void)destination; /* avoid compiler complaint about unused parameter */
-  KASSERT(intersectionSem != NULL);
-  P(intersectionSem);
+  kprintf("COUNT: %d\n", count);
+  kprintf("%d --> %d\n", origin, destination);
+  numCars++;
+  kprintf("NUM CARS: %d\n", numCars);
+  lock_acquire(action);
+  count++;
+  node *n;
+  n = (node *)kmalloc(sizeof(node));
+  n->next = 0;
+  n->path = (int)(origin*10 + destination);
+  if (!list_created) {
+    head = n;
+    tail = n;
+    list_created = true;
+  } else {
+    tail->next = n;
+    tail = tail->next;
+  }
+  if (count > 1) { 
+    cv_wait(paths[origin*10 + destination], action);
+  }
+  if (count == 1) {
+    lock_acquire(intersection);
+    bool l_turn;
+    bool r_turn;
+    bool straight;
+    
+    int diff = (int)(destination - origin);
+    if (diff == 1 || diff == -3) l_turn = true;
+    else if (diff == 2 || diff == -2) straight = true;
+    else r_turn = true;
+    
+    int d = (destination + 1) % 4;
+    if ((int)origin != d)
+      cv_broadcast(paths[origin*10 + d], intersection);
+    d = (destination + 2) % 4;
+    if ((int)origin != d)
+      cv_broadcast(paths[origin*10 + d], intersection);
+    d = (destination + 3) % 4;
+    if ((int)origin != d)
+      cv_broadcast(paths[origin*10 + d], intersection);
+    cv_broadcast(paths[destination*10 + origin], intersection);
+  }
+  lock_release(action);
 }
 
 
@@ -97,9 +162,21 @@ intersection_before_entry(Direction origin, Direction destination)
 void
 intersection_after_exit(Direction origin, Direction destination) 
 {
-  /* replace this default implementation with your own implementation */
-  (void)origin;  /* avoid compiler complaint about unused parameter */
-  (void)destination; /* avoid compiler complaint about unused parameter */
-  KASSERT(intersectionSem != NULL);
-  V(intersectionSem);
+  (void)origin;
+  (void)destination;
+  lock_acquire(action);
+  count--;
+  if (count == 0) {
+    lock_release(intersection);
+    while (head != NULL && count == 0) {
+      int next = head->path;
+      kprintf("=========\nNEXT: %d\n=========\n", next);
+      static volatile node *temp;
+      temp = head;
+      head = head->next;
+      kfree((void*)temp);
+      cv_broadcast(paths[next], action);
+    }
+  }
+  lock_release(action);
 }
